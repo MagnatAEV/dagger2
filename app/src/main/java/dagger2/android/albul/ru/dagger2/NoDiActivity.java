2a72b5089ac1d681dc2940e5b55fc0dab3836ab4
Evgeniy <evgeniyalbul@gmail.com>
2019-10-26 12:09:10 +0300

    create app

---
+++ app/src/main/java/dagger2/android/albul/ru/dagger2/NoDiActivity.java
@@ -0,0 +1,13 @@
+package dagger2.android.albul.ru.dagger2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoDiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_di);
+    }
+}
