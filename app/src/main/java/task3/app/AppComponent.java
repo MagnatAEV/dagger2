47c47f1217f6589c1f94f3d802ed7b6f37545a31
Evgeniy <evgeniyalbul@gmail.com>
2019-10-27 15:01:43 +0300

    ERROR: Gradle DSL method not found: 'app()'

--- app/src/main/java/task3/app/AppComponent.java
+++ app/src/main/java/task3/app/AppComponent.java
@@ -4,10 +4,12 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import task3.DaggerActivity;
+import task3.Red;
+import task3.White;
 
 @Singleton
 @Component(modules = {AppModule.class})
-public class AppComponent {
+public interface AppComponent {
 
     void inject(DaggerActivity daggerActivity);
 
